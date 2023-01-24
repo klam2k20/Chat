18f35d3f2a2acdbbd26d2ff2725a4824db789d69
klam2k20 <klam2k17@gmail.com>
2023-01-24 01:53:25 -0500

    Add cors to express

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config({ path: 'config.env' });
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware
+app.use(cors());
 app.use(require('./routes/router'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
