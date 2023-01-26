51797c549683dde669c23745dd90f23998cdaa0b
klam2k20 <klam2k17@gmail.com>
2023-01-26 14:31:02 -0500

    Add controller to add group chats, rename chats, and add and remove user from group chat

--- server/controller/chatController.js
+++ server/controller/chatController.js
@@ -1,8 +1,8 @@
+const { mongoose } = require('mongoose');
 const { Chat } = require('../model/models');
 
 const createChat = async (req, res) => {
   const errorMsg = 'Creating a Chat Requires a User ID';
-  if (!Object.keys(req.body).length) return res.status(400).json({ message: errorMsg });
   const { userId } = req.body;
   if (!userId) return res.status(400).json({ message: errorMsg });
   const isChat = await Chat.find({
@@ -16,7 +16,7 @@ const createChat = async (req, res) => {
   // path: 'latestMessage.sender',
   // select: 'name pic email',
   // });
-  if (isChat.length > 0) return res.status(200).json(isChat);
+  if (isChat.length) return res.status(200).json(isChat);
 
   const chat = Chat({ chatName: 'sender', users: [req.user._id, userId] });
   await chat.save(async (err) => {
@@ -44,32 +44,89 @@ const getUserChats = async (req, res) => {
   res.status(200).json(chats);
 };
 
-const createGroupChat = (req, res) => {
-  const errorMsg = 'Creating a Group Chat Requires a Chat Name and a List of User IDs';
-  if (!Object.keys(req.body).length) return res.status(400).json({ message: errorMsg });
+const createGroupChat = async (req, res) => {
+  const missingRequiredMsg = 'Creating a Group Chat Requires a Chat Name and a List of User IDs';
+  const userIdsLengthMsg = 'Creating a Group Chat Requires at Least 2 Additional User IDs';
   const { chatName, userIds } = req.body;
-  if (!chatName || !userIds) return res.status(400).json({ message: errorMsg });
+  if (!chatName || !userIds) return res.status(400).json({ message: missingRequiredMsg });
+  const allUserIds = JSON.parse(userIds);
+  const allObjectIds = allUserIds.map((id) => mongoose.Types.ObjectId(id));
+  allObjectIds.push(req.user._id);
+  if (allObjectIds.length < 3) return res.status(400).json({ message: userIdsLengthMsg });
+
+  const isGroupChat = await Chat.find({
+    isGroupChat: true,
+    $and: [
+      { users: { $size: allObjectIds.length } },
+      { users: { $all: allObjectIds } },
+    ],
+  })
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password')
+    .populate('latestMessage', '-chat');
+  if (isGroupChat.length) return res.status(200).json(isGroupChat);
+
+  const groupChat = Chat({
+    chatName,
+    groupChat: true,
+    users: [...allUserIds, req.user._id.toString()],
+    groupAdmin: req.user._id.toString(),
+  });
+  groupChat.save(async (err) => {
+    if (!err) {
+      const chat = await Chat.find({ _id: groupChat._id })
+        .populate('users', '-password')
+        .populate('groupAdmin', '-password');
+      return res.status(201).json(chat);
+    }
+    return res.status(400).json({ message: `Error While Creating Group Chat ${err.message}` });
+  });
 };
 
-const renameChat = (req, res) => {
-  const errorMsg = 'Renaming a Group Chat Requires a New Name and a Chat ID';
-  if (!Object.keys(req.body).length) return res.status(400).json({ message: errorMsg });
+const renameChat = async (req, res) => {
+  const errorMsg = 'Renaming a Chat Requires a Chat ID and Chat Name';
   const { chatId, chatName } = req.body;
   if (!chatId || !chatName) return res.status(400).json({ message: errorMsg });
+  const chat = await Chat.findByIdAndUpdate(
+    chatId,
+    { chatName },
+    { new: true },
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+  if (!chat) return res.status(404).json('Chat Not Found');
+  // Status Code 204 Doesn't Return Anything
+  return res.json(chat);
 };
 
-const addToGroup = (req, res) => {
+const addToGroup = async (req, res) => {
   const errorMsg = 'Adding a Member to a Chat Requires a Chat ID and User ID';
-  if (!Object.keys(req.body).length) return res.status(400).json({ message: errorMsg });
   const { chatId, userId } = req.body;
   if (!chatId || !userId) return res.status(400).json({ message: errorMsg });
+  const chat = await Chat.findByIdAndUpdate(
+    chatId,
+    { $addToSet: { users: userId } },
+    { new: true },
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+  if (!chat) return res.status(404).json('Chat Not Found');
+  return res.json(chat);
 };
 
-const removeFromGroup = (req, res) => {
+const removeFromGroup = async (req, res) => {
   const errorMsg = 'Removing a Member from a Chat Requires a Chat ID and User ID';
-  if (!Object.keys(req.body).length) return res.status(400).json({ message: errorMsg });
   const { chatId, userId } = req.body;
   if (!chatId || !userId) return res.status(400).json({ message: errorMsg });
+  const chat = await Chat.findByIdAndUpdate(
+    chatId,
+    { $pull: { users: userId } },
+    { new: true },
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+  if (!chat) return res.status(404).json('Chat Not Found');
+  return res.json(chat);
 };
 
 module.exports = {
