fd273f171cc9c53dad2817268cf9a9d9c6c6cbcd
klam2k20 <klam2k17@gmail.com>
2023-01-26 14:32:00 -0500

    Update expiration time for JWT

--- server/utilities/auth.js
+++ server/utilities/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-const generateJWT = (_id) => jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+const generateJWT = (_id) => jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
 module.exports = { generateJWT };
