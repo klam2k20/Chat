527c22c56e5dedfdd0b35803b96d9ed02dc157a3
klam2k20 <klam2k17@gmail.com>
2023-01-25 19:33:04 -0500

    Update signup input ids and add submit form function

--- client/src/Components/Auth/Signup.jsx
+++ client/src/Components/Auth/Signup.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+import axios from 'axios';
 import {
   Button,
   FormControl,
@@ -10,16 +10,18 @@ import {
   useToast,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [photo, setPhoto] = useState('');
+  const [setPhoto] = useState('');
   const [show, setShow] = useState(false);
   const [loading, setLoading] = useState(false);
   const toast = useToast();
+  const navigate = useNavigate();
 
   const toggleShow = () => {
     setShow(!show);
@@ -42,34 +44,92 @@ function Signup() {
         }
       }).catch((err) => {
         toast({
-          title: 'Error Uploading Photo! Please Try Again!',
+          title: `Error Uploading Photo: ${err.message}`,
           status: 'error',
           duration: 5000,
           isClosable: true,
           position: 'bottom',
         });
+        setLoading(false);
       });
   };
 
-  const submitForm = () => {
-
+  const submitForm = async () => {
+    setLoading(true);
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: 'Please Enter All Required Fields',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      setLoading(false);
+    } else if (password !== confirmPassword) {
+      toast({
+        title: 'Passwords Don\'t Match',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      setLoading(false);
+    } else {
+      const headers = { 'content-type': 'application/json' };
+      try {
+        const response = await axios.post(
+          'http://localhost:8080/api/user/',
+          { name, email, password },
+          headers,
+        );
+        toast({
+          title: 'Registration Successful!',
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+          position: 'bottom',
+        });
+        localStorage.setItem('user-info', JSON.stringify(response));
+        setLoading(false);
+        navigate('/chats');
+      } catch (err) {
+        toast({
+          title: 'Regristration Error',
+          description: err.message,
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+          position: 'bottom',
+        });
+        setLoading(false);
+      }
+    }
   };
 
   return (
     <VStack spacing="0.5rem" mt="0.5rem">
-      <FormControl id="name" isRequired>
+      <FormControl id="signup-name" isRequired>
         <FormLabel>Name</FormLabel>
-        <Input type="text" onChange={(e) => setName(e.target.value)} />
+        <Input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </FormControl>
-      <FormControl id="email" isRequired>
+      <FormControl id="signup-email" isRequired>
         <FormLabel>Email</FormLabel>
-        <Input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <Input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </FormControl>
-      <FormControl id="password" isRequired>
+      <FormControl id="signup-password" isRequired>
         <FormLabel>Password</FormLabel>
         <InputGroup>
           <Input
             type={show ? 'text' : 'password'}
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           <InputRightAddon>
@@ -79,11 +139,12 @@ function Signup() {
           </InputRightAddon>
         </InputGroup>
       </FormControl>
-      <FormControl id="confirmPassword" isRequired>
+      <FormControl id="signup-confirmPassword" isRequired>
         <FormLabel>Confirm Password</FormLabel>
         <InputGroup>
           <Input
             type={show ? 'text' : 'password'}
+            value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
           <InputRightAddon>
@@ -93,7 +154,7 @@ function Signup() {
           </InputRightAddon>
         </InputGroup>
       </FormControl>
-      <FormControl id="photo">
+      <FormControl id="signup-photo">
         <FormLabel>Upload Photo</FormLabel>
         <Input
           variant="unstyled"
