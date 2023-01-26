48bfb0476312bbb1b40503a319efa95051bc0647
klam2k20 <klam2k17@gmail.com>
2023-01-26 14:31:47 -0500

    Import controller functions instead of controller

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const controller = require('../controller/userController');
+const { registerUser, filterUsers, loginUser } = require('../controller/userController');
 const { authenicateToken } = require('../middleware/middleware');
 
-router.route('/').post(controller.registerUser)
-  .get(authenicateToken, controller.filterUsers);
+router.route('/').post(registerUser)
+  .get(authenicateToken, filterUsers);
 
-router.route('/login').post(controller.loginUser);
+router.route('/login').post(loginUser);
 
 module.exports = router;
