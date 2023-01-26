bd7f65a92ebbad01ec7da0434aad5d12c5c3733e
klam2k20 <klam2k17@gmail.com>
2023-01-25 21:58:38 -0500

    Add middleware to check if user has been logged in

--- server/utilities/auth.js
+++ server/utilities/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-const generateJWT = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
+const generateJWT = (_id) => jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
 module.exports = { generateJWT };
