9ec6f7a9084bbf441272cd83a69c39b42da993b6
klam2k20 <klam2k17@gmail.com>
2023-01-28 01:41:44 -0500

    Add delete icon to chats

--- client/src/Components/ChatsList.jsx
+++ client/src/Components/ChatsList.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import {
-  Box, Text, Avatar, Button, useToast,
+  Box, Text, Avatar, Button, useToast, ButtonGroup, IconButton,
 } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
+import { DeleteIcon } from '@chakra-ui/icons';
 import { getChatname } from '../Utilities/utilities';
 import { useChat } from '../Context/ChatProvider';
 import { getChats } from '../Utilities/apiRequests';
@@ -54,28 +55,34 @@ function Chat({ chat }) {
   };
 
   return (
-    <Button
-      display="flex"
-      gap="0.5rem"
-      justifyContent="justify-start"
-      alignItems="center"
-      py="1.5rem"
-      px="1rem"
-      onClick={selectChat}
-      colorScheme="gray"
-      _hover={{ background: '#e0e0e0' }}
-      bg={selectedChat._id === chat._id ? '#e0e0e0' : '#f2f2f2'}
-      width="full"
-    >
-      <Avatar size={{ base: 'xs', md: 'sm' }} name={getChatname(user, chat)} />
-      <Box display="flex" flexDirection="column" alignItems="start">
-        <Text fontSize="lg">{getChatname(user, chat)}</Text>
-        <Text fontSize="lg" fontWeight="lighter">
-          {/* {chat.latestMessage} */}
-          Latest Message
-        </Text>
-      </Box>
-    </Button>
+    <ButtonGroup size="lg" isAttached>
+      <Button
+        display="flex"
+        gap="0.5rem"
+        justifyContent="justify-start"
+        alignItems="center"
+        py="1.5rem"
+        px="1rem"
+        onClick={selectChat}
+        colorScheme="gray"
+        _hover={{ background: '#e0e0e0' }}
+        bg={selectedChat && selectedChat._id === chat._id ? '#e0e0e0' : '#f2f2f2'}
+        width="full"
+      >
+        <Avatar
+          size={{ base: 'xs', md: 'sm' }}
+          name={getChatname(user, chat)}
+        />
+        <Box display="flex" flexDirection="column" alignItems="start">
+          <Text fontSize="lg">{getChatname(user, chat)}</Text>
+          <Text fontSize="lg" fontWeight="lighter">
+            {/* {chat.latestMessage} */}
+            Latest Message
+          </Text>
+        </Box>
+      </Button>
+      <IconButton _hover={{ color: 'white' }} bg="red.500" icon={<DeleteIcon />} />
+    </ButtonGroup>
   );
 }
 
